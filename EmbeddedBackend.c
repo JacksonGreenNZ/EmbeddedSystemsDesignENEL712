36f306edb26bb0bba1972f3404f051b36f2ec028
Jackson Green <87842000+JacksonGreenNZ@users.noreply.github.com>
2025-03-05 12:56:22 +1300

    Create EmbeddedBackend.c

---
+++ EmbeddedBackend.c
@@ -0,0 +1,17 @@
+/*
+ * EmbeddedBackend.c
+ *
+ * Created: 5/03/2025 12:49:56 pm
+ * Author : qtr1230
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
